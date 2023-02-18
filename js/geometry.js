c27d4b56e629edc87565d26e0d7db3f394ca9925
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-02-18 20:02:11 +0600

    Update geometry.js

--- js/geometry.js
+++ js/geometry.js
@@ -113,15 +113,13 @@ document.getElementById('para-calculate').addEventListener('click', function () 
 // PART 01 END:::::::::::
 
 
-
-
+// PART 02 START TRIANGLE,RHOMBOS & PENTAGOAN IN ONE FUNCTION::::
 // MADE FUNCTION FOR TRIANGLE, RHOMBOS, PENTAGOAN::
 function AreaOfTriRhoPen(inputs1,inputs2,base1,height1,heading1){
     const triangleInputFieldFloat1 = parseFloat(inputs1); //CHANGE
     const triangleInputFieldFloat2 = parseFloat(inputs2); //CHANGE
 
     // CALCULATION PART::
-
     const triangleAreaResult = 0.5 * triangleInputFieldFloat1 * triangleInputFieldFloat2;
 
     const tribase = document.getElementById(base1); //CHANGE
@@ -133,10 +131,10 @@ function AreaOfTriRhoPen(inputs1,inputs2,base1,height1,heading1){
     //   Fetch Name From UI;
     const triHeading = document.getElementById(heading1); //CHANGE
     const triHeadingText = triHeading.innerText;
+
     //   RESULT SHOWING PART;
     const name = document.getElementById('name');
     const result = document.getElementById('result');
-
     name.innerText = triHeadingText;
     result.innerText = triangleAreaResult.toFixed(2);
 
@@ -151,17 +149,14 @@ function AreaOfTriRhoPen(inputs1,inputs2,base1,height1,heading1){
         const cm = document.getElementById('c');
         cm.style.display = 'none';
     })
-
     // FOR CROSS BUTTON DELETE THE FULL ELEMENT OF RESULT::
     document.getElementById('cross-btn').addEventListener('click', function () {
         olList.style.display = 'none';
     })
-
 }
 
 // TRIANGALE INPUT BOX WORKING PROCEDURE::
 document.getElementById('triangle-calculate').addEventListener('click', function () {
-
     // First InputBox
     const triangleInputField1 = document.getElementById('tri-input1');
     const triangleInputFieldValue1 = triangleInputField1.value;
@@ -191,9 +186,7 @@ document.getElementById('triangle-calculate').addEventListener('click', function
         olList.style.display = 'none';
     }
     else {
-
         const areaOfTriangle = AreaOfTriRhoPen(triangleInputFieldValue1,triangleInputFieldValue2,'tri-base','tri-height','tri-heading');
-
         triangleInputField1.value = '';
         triangleInputField2.value = '';
     } //END OF ELSE CONDITION
@@ -230,95 +223,51 @@ const triangleInputFieldValue2 = triangleInputField2.innerText;
         const pentagonArea = AreaOfTriRhoPen(triangleInputFieldValue1,triangleInputFieldValue2,'penta-base','penta-height','penta-heading');
     }
 })
-
 // Working with RHOMBOS::
 document.getElementById('rombhos-calculate').addEventListener('click', function () {
-
     // First Rhombos InputBox
     const romInputField1 = document.getElementById('rom-value1');
     const romInputFieldValue1 = romInputField1.innerText;
-    const romInputFieldFloat1 = parseFloat(romInputFieldValue1);
-
     // SECOND Rhombos INPUT FIELD::
     const romInputField2 = document.getElementById('rom-value2');
     const romInputFieldValue2 = romInputField2.innerText;
-    const romInputFieldFloat2 = parseFloat(romInputFieldValue2);
-
     // Validation;
-    if (isNaN(PentagonInputFieldFloat1)) {
+    if (isNaN(romInputFieldValue1)) {
         alert('Please Enter valid Input...!');
         const olList = document.getElementById('ol-list');
         olList.style.display = 'none';
     }
-    else if (isNaN(PentagonInputFieldFloat2)) {
+    else if (isNaN(romInputFieldValue2)) {
         alert('Please Enter valid Input...!');
         const olList = document.getElementById('ol-list');
         olList.style.display = 'none';
     }
-
     else {
-
         // CALCULATION PART::
-
-        const triangleAreaResult = 0.5 * romInputFieldFloat1 * romInputFieldValue2;
-
-        const tribase = document.getElementById('rom-base');
-        tribase.innerText = romInputFieldFloat1;
-
-        const triheight = document.getElementById('rom-height');
-        triheight.innerText = romInputFieldFloat2;
-
-        //   Fetch Name From UI;
-        const triHeading = document.getElementById('rom-heading');
-        const PentaHeadingText = triHeading.innerText;
-        //   RESULT SHOWING PART;
-        const name = document.getElementById('name');
-        const result = document.getElementById('result');
-
-        name.innerText = PentaHeadingText;
-        result.innerText = triangleAreaResult.toFixed(2);
-
-        // SHOWING THE RESULT IN A BLOCK::
-        const olList = document.getElementById('ol-list');
-        olList.style.display = 'block';
-
-        // Convert to Meter square::
-        document.getElementById('convert').addEventListener('click', function () {
-            const meterSquareResult = triangleAreaResult / 10000;
-            result.innerText = meterSquareResult.toFixed(3); //I took toFixed(3) because sometimes value comes 0.00 if i take toFixed(2);
-            const cm = document.getElementById('c');
-            cm.style.display = 'none';
-        })
-
-        // FOR CROSS BUTTON DELETE THE FULL ELEMENT OF RESULT::
-        document.getElementById('cross-btn').addEventListener('click', function () {
-            olList.style.display = 'none';
-        })
-
+        const rhombosArea = AreaOfTriRhoPen(romInputFieldValue1,romInputFieldValue2,'rom-base','rom-height','rom-heading');
     }
-
 })
 
 // WORKING WITH ELLIPSE::
 document.getElementById('ell-calculate').addEventListener('click', function () {
 
     // First Rhombos InputBox
-    const romInputField1 = document.getElementById('ell-value1');
-    const romInputFieldValue1 = romInputField1.innerText;
-    const romInputFieldFloat1 = parseFloat(romInputFieldValue1);
+    const ellInputField1 = document.getElementById('ell-value1');
+    const ellInputFieldValue1 = ellInputField1.innerText;
+    const ellInputFieldFloat1 = parseFloat(ellInputFieldValue1);
 
     // SECOND Rhombos INPUT FIELD::
-    const romInputField2 = document.getElementById('ell-value2');
-    const romInputFieldValue2 = romInputField2.innerText;
-    const romInputFieldFloat2 = parseFloat(romInputFieldValue2);
+    const ellInputField2 = document.getElementById('ell-value2');
+    const ellInputFieldValue2 = ellInputField2.innerText;
+    const ellInputFieldFloat2 = parseFloat(ellInputFieldValue2);
 
     // Validation;
-    if (isNaN(PentagonInputFieldFloat1)) {
+    if (isNaN(ellInputFieldValue1)) {
         alert('Please Enter valid Input...!');
         const olList = document.getElementById('ol-list');
         olList.style.display = 'none';
     }
-    else if (isNaN(PentagonInputFieldFloat2)) {
+    else if (isNaN(ellInputFieldValue2)) {
         alert('Please Enter valid Input...!');
         const olList = document.getElementById('ol-list');
         olList.style.display = 'none';
@@ -328,22 +277,22 @@ document.getElementById('ell-calculate').addEventListener('click', function () {
 
         // CALCULATION PART::
 
-        const triangleAreaResult = 3.1416 * romInputFieldFloat1 * romInputFieldValue2;
+        const triangleAreaResult = 3.1416 * ellInputFieldFloat1 * ellInputFieldFloat2;
 
         const tribase = document.getElementById('ell-base');
-        tribase.innerText = romInputFieldFloat1;
+        tribase.innerText = ellInputFieldValue1;
 
         const triheight = document.getElementById('ell-height');
-        triheight.innerText = romInputFieldFloat2;
+        triheight.innerText = ellInputFieldValue2;
 
         //   Fetch Name From UI;
         const triHeading = document.getElementById('ell-heading');
-        const PentaHeadingText = triHeading.innerText;
+        const triHeadingText = triHeading.innerText;
         //   RESULT SHOWING PART;
         const name = document.getElementById('name');
         const result = document.getElementById('result');
 
-        name.innerText = PentaHeadingText;
+        name.innerText = triHeadingText;
         result.innerText = triangleAreaResult.toFixed(2);
 
         // SHOWING THE RESULT IN A BLOCK::
