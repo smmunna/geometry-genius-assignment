4f2453418dee6f7dc70663d0457374fc4e8c7a81
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-02-18 10:10:54 +0600

    Triangle Calculation Complete

--- js/geometry.js
+++ js/geometry.js
@@ -8,6 +8,31 @@ document.getElementById('triangle-calculate').addEventListener('click',function(
     const triangleInputFieldValue2 = triangleInputField2.value;
     const triangleInputFieldFloat2 = parseFloat(triangleInputFieldValue2);
     
+    // Validation;
+    if(isNaN(triangleInputFieldValue1)){
+        alert('Please Enter valid Input...!');
+        const olList = document.getElementById('ol-list');
+        olList.style.display='none';
+    }
+    else if(isNaN(triangleInputFieldValue2)){
+        alert('Please Enter valid Input...!');
+        const olList = document.getElementById('ol-list');
+        olList.style.display='none';
+    }
+    else if(triangleInputFieldValue1===''){
+        alert('Please enter any input...!');
+        const olList = document.getElementById('ol-list');
+        olList.style.display='none';
+    }
+    else if(triangleInputFieldValue2===''){
+        alert('Please enter any input...!');
+        const olList = document.getElementById('ol-list');
+        olList.style.display='none';
+    }
+    else{
+
+
+
    const triangleAreaResult = 0.5*triangleInputFieldFloat1*triangleInputFieldFloat2;
    
 
@@ -20,8 +45,18 @@ document.getElementById('triangle-calculate').addEventListener('click',function(
   const triheight = document.getElementById('tri-height');
   triheight.innerText = triangleInputFieldValue2;
 
+//   Fetch Name From UI;
+const triHeading = document.getElementById('tri-heading');
+const triHeadingText = triHeading.innerText;
+//   RESULT SHOWING PART;
+const name = document.getElementById('name');
+const result = document.getElementById('result');
 
-  
+name.innerText = triHeadingText;
+result.innerText = triangleAreaResult;
 
+const olList = document.getElementById('ol-list');
+olList.style.display='block';  
 
+    }
 })
