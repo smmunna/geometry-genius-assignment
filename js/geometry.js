b2afd3968000eae43fdf5a837cfcdd269a8704b3
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-02-18 14:36:54 +0600

    update

--- js/geometry.js
+++ js/geometry.js
@@ -94,3 +94,79 @@ const triangleInputFieldFloat2 = parseFloat(triangleInputFieldValue2);
 const findAreaofTRriangle = findAreaOfThree(triangleInputFieldFloat1,triangleInputFieldFloat2);
 
 })
+
+
+
+// FINDING THE RHOMBOS AREA::
+
+function findingArea(romValue1,romValue2){
+ 
+
+ // Validation;
+ if (isNaN(triangleInputFieldFloat1)) {
+    alert('Please Enter valid Input...!');
+    const olList = document.getElementById('ol-list');
+    olList.style.display = 'none';
+}
+else if (isNaN(triangleInputFieldFloat2)) {
+    alert('Please Enter valid Input...!');
+    const olList = document.getElementById('ol-list');
+    olList.style.display = 'none';
+}
+
+else {
+
+    // CALCULATION PART::
+
+    const triangleAreaResult = 0.5 * romValue1 * romValue2;
+
+    const tribase = document.getElementById('rom-base');
+    tribase.innerText = triangleInputFieldFloat1;
+
+    const triheight = document.getElementById('rom-height');
+    triheight.innerText = triangleInputFieldFloat2;
+
+    //   Fetch Name From UI;
+    const triHeading = document.getElementById('rom-heading');
+    const triHeadingText = triHeading.innerText;
+    //   RESULT SHOWING PART;
+    const name = document.getElementById('name');
+    const result = document.getElementById('result');
+
+    name.innerText = triHeadingText;
+    result.innerText = triangleAreaResult.toFixed(2);
+
+    // SHOWING THE RESULT IN A BLOCK::
+    const olList = document.getElementById('ol-list');
+    olList.style.display = 'block';
+
+    // Convert to Meter square::
+    document.getElementById('convert').addEventListener('click', function () {
+        const meterSquareResult = triangleAreaResult / 10000;
+        result.innerText = meterSquareResult.toFixed(3); //I took toFixed(3) because sometimes value comes 0.00 if i take toFixed(2);
+        const cm = document.getElementById('c');
+        cm.style.display='none';
+    })
+
+    // FOR CROSS BUTTON DELETE THE FULL ELEMENT OF RESULT::
+    document.getElementById('cross-btn').addEventListener('click', function () {
+        olList.style.display = 'none';
+    })
+
+} 
+}
+
+// First RHOMBOS InputBox
+const triangleInputField1 = document.getElementById('rom-value1');
+const triangleInputFieldValue1 = triangleInputField1.innerText;
+const triangleInputFieldFloat1 = parseFloat(triangleInputFieldValue1);
+
+// SECOND RHOMBOS INPUT FIELD::
+const triangleInputField2 = document.getElementById('rom-value2');
+const triangleInputFieldValue2 = triangleInputField2.innerText;
+const triangleInputFieldFloat2 = parseFloat(triangleInputFieldValue2);
+// WORKING WITH RHOMBOS::
+document.getElementById('rombhos-calculate').addEventListener('click',function(){
+// PASSING THE VALUES TO THE FUNCTION;
+const findAreaofRhombos = findingArea(triangleInputFieldFloat1,triangleInputFieldFloat2);
+})
